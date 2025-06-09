d5273988788b87c05e9e1eff6346384d9547ad4e
Wolf Vollprecht <w.vollprecht@gmail.com>
2025-06-09 20:17:48 +0200

    try a v5 build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,30 +1,18 @@
-#!/bin/bash
-# Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/gnuconfig/config.* ./dist
+export CFLAGS="${CFLAGS} -Wno-implicit-function-declaration"
 
-if [[ $(uname) == Darwin ]]; then
-    export CFLAGS="$CFLAGS -include stddef.h"
-    export CXXFLAGS="-stdlib=libc++ $CXXFLAGS"
-fi
-
+# BerkeleyDB requires you to build everything from the build_unix subdirectory
 cd build_unix
-../dist/configure --prefix=$PREFIX \
-                  --enable-shared \
-                  --disable-static \
-                  --disable-debug \
-                  --enable-cxx \
-                  --enable-stl \
-                  --enable-compat185 \
-                  --enable-sql \
-                  --enable-sql_codegen \
-                  --enable-dbm \
+../dist/configure \
+    --disable-static \
+    --prefix=$PREFIX \
+    --mandir=$PREFIX/share/man \
+    --enable-cxx \
+    --enable-dbm
 
+make
+make install DOCLIST=license
 
-make -j${CPU_COUNT}
-if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
-    make check -j${CPU_COUNT}
-fi
-make install -j${CPU_COUNT} DOCLIST=license
-
-cd $PREFIX
-find . -type f -name "*.la" -exec rm -rf '{}' \; -print
+# use the standard docs location
+mkdir -p $PREFIX/share/doc
+mv $PREFIX/docs $PREFIX/share/doc/berkeley-db
